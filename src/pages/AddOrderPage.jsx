9f05507c458d5a7e79aced1cc8ee3db2532c0d19
Sk <techjk021@gmail.com>
2025-01-29 03:29:02 +0530

    Edit Page

--- src/pages/AddOrderPage.jsx
+++ src/pages/AddOrderPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import {
   Box,
@@ -11,52 +11,92 @@ import {
   TableCell,
   TableHead,
   TableRow,
+  Select,
+  MenuItem,
 } from "@mui/material";
 import { FaCopy, FaTrash, FaPlus } from "react-icons/fa";
 import "../css/main.css";
+import { useAuth } from "../routes/AuthContext";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export default function AddOrderPage({}) {
+export default function AddOrderPage({ edit }) {
   const { id } = useParams();
   const [items, setItems] = useState([]);
+  const [oldItems, setOldItems] = useState([]);
   const [counter, setCounter] = useState(1);
   const [senderDetails, setSenderDetails] = useState({ role: "sender" });
   const [receiverDetails, setReceiverDetails] = useState({ role: "receiver" });
   const [error, setError] = useState(false);
   const [charges, setCharges] = useState(0);
+  const [status, setStatus] = useState(0);
+  const [sourceWarehouse, setSourceWarehouse] = useState("");
+  const [destinationWarehouse, setDestWarehouse] = useState("");
+  const [initialitems, setInitialItems] = useState(0);
+  const { isAdmin, isSource } = useAuth();
+
   const navigate = useNavigate();
 
+  useEffect(() => {
+    if (edit) fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const token = localStorage.getItem("token");
+    const response = await fetch(`${BASE_URL}/api/parcel/track/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      alert("Error occurred");
+      return;
+    }
+    if (response.status === 201) {
+      alert("No such Order");
+      return;
+    }
+    const data = (await response.json()).body;
+    console.log(data);
+    setSenderDetails(data.sender);
+    setReceiverDetails(data.receiver);
+    setSourceWarehouse(data.sourceWarehouse.warehouseID);
+    setDestWarehouse(data.destinationWarehouse.warehouseID);
+    setOldItems(data.items);
+    setInitialItems(data.items.length);
+  };
+
   const handleAddRow = () => {
-    setItems([...items, { id: counter, name: "", quantity: "" }]);
+    setItems([...items, { itemId: counter, name: "", quantity: "" }]);
     setCounter(counter + 1);
   };
 
-  const handleRemoveRow = (id) => {
+  const handleRemoveRow = (itemId) => {
     const updatedItems = items
-      .filter((item) => item.id !== id)
+      .filter((item) => item.itemId !== itemId)
       .map((item, index) => ({
         ...item,
-        id: index + 1,
+        itemId: index + 1,
       }));
     setItems(updatedItems);
     setCounter(updatedItems.length + 1);
   };
 
-  const handleCopyRow = (id) => {
-    const itemToCopy = items.find((item) => item.id === id);
+  const handleCopyRow = (itemId) => {
+    const itemToCopy = items.find((item) => item.itemId === itemId);
     if (itemToCopy) {
-      setItems([...items, { ...itemToCopy, id: counter }]);
+      setItems([...items, { ...itemToCopy, itemId: counter }]);
       setCounter(counter + 1);
     }
   };
 
-  const handleInputChange = (id, field, value) => {
+  const handleInputChange = (itemId, field, value) => {
     if (field === "quantity") {
       value = parseInt(value);
     }
     const updatedItems = items.map((item) =>
-      item.id === id ? { ...item, [field]: value } : item
+      item.itemId === itemId ? { ...item, [field]: value } : item
     );
     setItems(updatedItems);
   };
@@ -82,8 +122,8 @@ export default function AddOrderPage({}) {
     }
 
     if (
-      items.length === 0 ||
-      items.some((item) => !item.name || !item.quantity)
+      oldItems.length === 0 &&
+      (items.length === 0 || items.some((item) => !item.name || !item.quantity))
     ) {
       return false;
     }
@@ -123,12 +163,15 @@ export default function AddOrderPage({}) {
       });
   };
 
-  const renderTextField = ({ label, name, isOptional, isSender }) => (
+  const renderTextField = ({ label, name, isOptional, isSender, value }) => (
     <TextField
-      label={label}
+      // label={label}
+      placeholder={label}
       variant="outlined"
       fullWidth
+      disabled={edit && !isAdmin}
       name={name}
+      value={value}
       onChange={(e) => handleDetailsChange(e, isSender ? "sender" : "receiver")}
       error={
         error &&
@@ -162,7 +205,8 @@ export default function AddOrderPage({}) {
         variant="h4"
         sx={{ marginBottom: "20px", textAlign: "center", color: "#1c3553" }}
       >
-        Add Order
+        {edit ? "Edit " : "Add "}
+        Order
       </Typography>
 
       <Box
@@ -176,33 +220,40 @@ export default function AddOrderPage({}) {
           label: "Sender's Name",
           name: "name",
           isSender: true,
+          value: senderDetails.name,
         })}
         {renderTextField({
           label: "Receiver's Name",
           name: "name",
           isSender: false,
+          value: receiverDetails.name,
         })}
         {renderTextField({
           label: "Sender's Phone No.",
           name: "phoneNo",
           isSender: true,
+          value: senderDetails.phoneNo,
         })}
         {renderTextField({
           label: "Receiver's Phone No.",
           name: "phoneNo",
           isSender: false,
+          value: receiverDetails.phoneNo,
         })}
         {renderTextField({
           label: "Sender's Address (Optional)",
           name: "address",
           isSender: true,
           isOptional: true,
+          value: senderDetails.address,
         })}
         {renderTextField({
           label: "Receiver's Address",
           name: "address",
           isSender: false,
+          value: receiverDetails.address,
         })}
+
         <TextField
           label="Charges"
           variant="outlined"
@@ -214,7 +265,201 @@ export default function AddOrderPage({}) {
             "& .MuiInputLabel-root": { fontSize: "14px", color: "#7d8695" },
           }}
         />
+        {isAdmin ? (
+          <Select
+          id="sourceWarehouse-select"
+          variant="outlined"
+          value={sourceWarehouse}
+          onChange={(event) => setSourceWarehouse(event.target.value)}
+          fullWidth
+          style={{
+            textAlign: "left",
+            background: "#f9f9f9",
+            borderRadius: "8px",
+            display: !isAdmin ? "none" : "initial",
+          }}
+          sx={{
+            "& .MuiInputBase-root": { fontSize: "14px", color: "#1b3655" },
+            "& .MuiInputLabel-root": { fontSize: "14px", color: "#7d8695" },
+          }}
+        >
+          <MenuItem value={0}>{"BHP"}</MenuItem>
+          <MenuItem value={1}>{"Two"}</MenuItem>
+          <MenuItem value={2}>{"Three"}</MenuItem>
+        </Select>
+        ) : null}
+        <Select
+          id="destWarehouse-select"
+          variant="outlined"
+          value={destinationWarehouse}
+          onChange={(event) => setDestWarehouse(event.target.value)}
+          fullWidth
+          style={{
+            textAlign: "left",
+            background: "#f9f9f9",
+            borderRadius: "8px",
+          }}
+          sx={{
+            "& .MuiInputBase-root": { fontSize: "14px", color: "#1b3655" },
+            "& .MuiInputLabel-root": { fontSize: "14px", color: "#7d8695" },
+          }}
+        >
+          <MenuItem value={0}>{"MNC"}</MenuItem>
+          <MenuItem value={1}>{"Two"}</MenuItem>
+          <MenuItem value={2}>{"Three"}</MenuItem>
+        </Select>
+        <Select
+          id="warehouse-select"
+          variant="outlined"
+          value={status}
+          onChange={(event) => setStatus(event.target.value)}
+          fullWidth
+          style={{
+            textAlign: "left",
+            background: "#f9f9f9",
+            borderRadius: "8px",
+            display: !edit || !isAdmin ? "none" : "",
+          }}
+          sx={{
+            "& .MuiInputBase-root": { fontSize: "14px", color: "#1b3655" },
+            "& .MuiInputLabel-root": { fontSize: "14px", color: "#7d8695" },
+          }}
+        >
+          <MenuItem value={0}>{"Arrived"}</MenuItem>
+          <MenuItem value={1}>{"Partial"}</MenuItem>
+          <MenuItem value={2}>{"Delivered"}</MenuItem>
+        </Select>
       </Box>
+
+      {edit ? (
+        <Box sx={{ marginTop: "30px" }}>
+          <Typography
+            variant="h6"
+            sx={{
+              marginBottom: "10px",
+              textAlign: "center",
+              color: "#25344e",
+              fontWeight: "bold",
+            }}
+          >
+            Old Items
+          </Typography>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ color: "#1b3655", fontWeight: "bold" }}>
+                  Sl. No.
+                </TableCell>
+                <TableCell sx={{ color: "#1b3655", fontWeight: "bold" }}>
+                  Item Name
+                </TableCell>
+                <TableCell sx={{ color: "#1b3655", fontWeight: "bold" }}>
+                  Quantity
+                </TableCell>
+                {isAdmin ? (
+                  <TableCell sx={{ color: "#1b3655", fontWeight: "bold" }}>
+                    Status
+                  </TableCell>
+                ) : null}
+                <TableCell sx={{ color: "#1b3655", fontWeight: "bold" }}>
+                  Actions
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {oldItems.map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell>{item.itemId}</TableCell>
+                  <TableCell>
+                    <TextField
+                      value={item.name}
+                      onChange={(e) =>
+                        handleInputChange(item.itemId, "name", e.target.value)
+                      }
+                      disabled={!isAdmin}
+                      placeholder="Enter Item Name"
+                      variant="outlined"
+                      size="small"
+                      fullWidth
+                      error={error && !item.name}
+                      helperText={error && !item.name ? "Required" : ""}
+                      sx={{
+                        "& .MuiInputBase-root": {
+                          fontSize: "14px",
+                          color: "#1b3655",
+                        },
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <TextField
+                      type="number"
+                      disabled={!isAdmin}
+                      value={item.quantity}
+                      onChange={(e) =>
+                        handleInputChange(
+                          item.itemId,
+                          "quantity",
+                          e.target.value
+                        )
+                      }
+                      variant="outlined"
+                      size="small"
+                      fullWidth
+                      error={error && !item.quantity}
+                      helperText={error && !item.quantity ? "Required" : ""}
+                      sx={{
+                        "& .MuiInputBase-root": {
+                          fontSize: "14px",
+                          color: "#1b3655",
+                        },
+                      }}
+                    />
+                  </TableCell>
+                  {isAdmin ? (
+                    <TableCell>
+                      <Select
+                        id="warehouse-select"
+                        variant="outlined"
+                        value={item.status}
+                        onChange={(event) => setStatus(event.target.value)}
+                        fullWidth
+                        style={{
+                          textAlign: "left",
+                          background: "#f9f9f9",
+                          borderRadius: "8px",
+                        }}
+                        sx={{
+                          "& .MuiInputBase-root": {
+                            fontSize: "14px",
+                            color: "#1b3655",
+                          },
+                          "& .MuiInputLabel-root": {
+                            fontSize: "14px",
+                            color: "#7d8695",
+                          },
+                        }}
+                      >
+                        <MenuItem value={0}>{"Arrived"}</MenuItem>
+                        <MenuItem value={1}>{"Partial"}</MenuItem>
+                        <MenuItem value={2}>{"Delivered"}</MenuItem>
+                      </Select>
+                    </TableCell>
+                  ) : null}
+                  <TableCell>
+                    <IconButton
+                      color="secondary"
+                      onClick={() => handleRemoveRow(item.itemId)}
+                    >
+                      <FaTrash />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Box>
+      ) : null}
 
       <Box sx={{ marginTop: "30px" }}>
         <Typography
@@ -246,15 +491,16 @@ export default function AddOrderPage({}) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {items.map((item) => (
-              <TableRow key={item.id}>
-                <TableCell>{item.id}</TableCell>
+            {items.map((item, index) => (
+              <TableRow key={index}>
+                <TableCell>{index + 1}</TableCell>
                 <TableCell>
                   <TextField
                     value={item.name}
                     onChange={(e) =>
-                      handleInputChange(item.id, "name", e.target.value)
+                      handleInputChange(item.itemId, "name", e.target.value)
                     }
+                    disabled={index < initialitems}
                     placeholder="Enter Item Name"
                     variant="outlined"
                     size="small"
@@ -272,9 +518,10 @@ export default function AddOrderPage({}) {
                 <TableCell>
                   <TextField
                     type="number"
+                    disabled={index < initialitems}
                     value={item.quantity}
                     onChange={(e) =>
-                      handleInputChange(item.id, "quantity", e.target.value)
+                      handleInputChange(item.itemId, "quantity", e.target.value)
                     }
                     variant="outlined"
                     size="small"
@@ -292,13 +539,13 @@ export default function AddOrderPage({}) {
                 <TableCell>
                   <IconButton
                     color="primary"
-                    onClick={() => handleCopyRow(item.id)}
+                    onClick={() => handleCopyRow(item.itemId)}
                   >
                     <FaCopy />
                   </IconButton>
                   <IconButton
                     color="secondary"
-                    onClick={() => handleRemoveRow(item.id)}
+                    onClick={() => handleRemoveRow(item.itemId)}
                   >
                     <FaTrash />
                   </IconButton>
@@ -316,7 +563,8 @@ export default function AddOrderPage({}) {
 
       <Box sx={{ textAlign: "center", marginTop: "30px" }}>
         <button className="button button-large" onClick={handleAddOrder}>
-          Add Order
+          {edit ? "Save " : "Add "}
+          Order
         </button>
       </Box>
     </Box>
